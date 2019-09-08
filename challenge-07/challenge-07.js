5ca967ba18ca7e282ccd952f69e27fea7c10bb29
Fabio Daemon <44199988+fabiodaemon@users.noreply.github.com>
2019-09-08 19:35:46 -0300

    completed

--- challenge-07/challenge-07.js
+++ challenge-07/challenge-07.js
@@ -1,32 +1,50 @@
 /*
 Crie um array com 5 items (tipos variados).
 
+RESPOSTA: var array = ['maca', 10, function funcao(){}, true, {propriedade: 'exemplo'}]
+
 
 Crie uma função chamada `addItem`, que irá adicionar itens no array criado.
 A função deverá retornar o array atualizado.
 
-
+RESPOSTA: function addItem(novoItem){
+    array.push(novoItem);
+    return array;
+}
 
 
 Adicione um novo array ao array criado no início do desafio, com ao menos 3
 itens de tipos diferentes, mostrando o resultado no console.
 
+RESPOSTA: addItem(['eu', false, function seiLa(){}]);
+            (6) ["maca", 10, ƒ, true, {…}, "eu"]
+            0: "maca"
+            1: 10
+            2: ƒ funcao()
+            3: true
+            4: {propriedade: "exemplo"}
+            5: "eu"
+            6: (3) ["eu", false, ƒ]
+            length: 7
+            __proto__: Array(0)
 
 
 Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 
-
+RESPOSTA: console.log('O segundo elemento do segundo array é ' + array[5][2]);
 
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 
+RESPOSTA: console.log('O primeiro array tem ' + array.length + ' itens');
 
 
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 
+RESPOSTA: console.log('O segundo array tem ' + array[5].length + ' itens');
 
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
